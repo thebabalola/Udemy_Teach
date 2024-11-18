d100b0a2343b2463561420692e9f87d5b0ed6b34
thebabalola <t.babalolajoseph@gmail.com>
2024-11-18 05:16:23 +0100

    query selector done

--- 2_Pablo_Farias-JQuery/script.js
+++ 2_Pablo_Farias-JQuery/script.js
@@ -17,7 +17,7 @@
 
 $(document).ready(function(){
 
-	//selecting by ID or Class 
+	// --- selecting by ID or Class  ---
 	$('#container').html('<strong>Hello!</strong>');
     $('.p2').text('hey! i am under Hello');
 
@@ -36,7 +36,7 @@ $(document).ready(function(){
 	$('input[placeholder*=Name]').addClass('highlight') //selected all input, with the Name placeholder
 
 
-	//Traversing the DOM
+	// --- Traversing the DOM ---
 	results = $('#animals').find('creature'); //this finds all the descendeant under animals
 	results = $('#animals').children('creature'); //gives the direct descendant of animals
 
@@ -53,7 +53,136 @@ $(document).ready(function(){
 	results = $('cat').closest('.category');  // picks the closest element to cat with the class .category(it could be the parent element or other elements)
 
 	
-	// Working with Events
+	// Working with Events (click events)
+	$('#example').on('click', function() {
+		$(this).addClass('highlighted1');  // that is, it adds the css ruleset of class highlighted to 'this', which is the ID #example
+		$(this).toggleClass('highlighted1'); //toggles between the color of highlighted & the default color of 'this' (#eample), when clicked
+	});
+
+				//Event delegation
+	$('exapmle').on('click', 'button.switch', function() { //that is, in the #example container, if there is a click on the 'button tag' with class 'switch', carry out the action in the function.
+		$(this).toggleClass('highlighted'); //woks exactly like the above, just that you have to click the button for the color to change
+	})	
+
+	$('e-example').on('click', 'button.pickBtn', function() {
+		let selected = $('#place option:selected');
+		let value = selected.val();
+
+		if(price) {
+			$('#result').html(value + ' ' + price);
+		}
+		
+	})
+
+				// Changing Events
+	$('e-example').on('change', 'select', function() {
+		let selected = $(this).find('option:selected');
+		let price = selected.data('price');
+
+		if(price) {
+			$('#result').html(value + ' ' + price);
+		}
+	});
+
+				//keydown, keyup
+	$('#example').on('keyup', 'input', function() {
+		$('#result').html('Your name is: ' + $(this).val());
+	});
+
+				// Preventing default Behaviout in Events.
+	$('#example').on('click', 'a', function(event) {
+		event.preventDefault();	//prevents pages from opening
+		event.stopPropagation();	
+		console.log('hello');
+	});
+
+	$('#example').on('click', function(event) {
+		console.log('hello container');
+	});
+
+
+	// --- DOM Manipulation ---
+	$('#add-container').on('click', 'button', function(){
+		var value = $('#add-container input').val();
+		console.log(value);
+
+		var html = '<div class="item">\
+                <div class="remove">X</div>' + value + '</div>';
+        console.log(html);
+
+			/* using prepend and Append */
+        //$('#places-container').append(html);
+        //$(html).appendTo('#places-container');
+
+        $('#places-container').prepend(html);	//adds new element to the begining of the list
+        //$(html).prependTo('#places-container');
+
+			/* using Before and After */
+		$('#places-container').children().first().before(html);
+
+			/* Removing DOM elements */
+		$('#places-container').on('click', '.remove', function(){
+		$(this).parent().remove();
+	});
+
+			/* DOM manipulation Empty()*/
+		//$('#container').text('<strong>Hello World</strong>');
+		$('#container').html('<strong>Hello World</strong>');
+
+		console.log($('#container').html());
+		//$('#container').empty();
+	
+		$('#container').attr('new-attribute', 123);
+		console.log($('#container').attr('new-attribute'));
+		});
+
+
+
+	/*  --- JQuery Animation --- */
+	$('.item-box').on('click', 'a.info-link', function(event){
+		event.preventDefault();
+
+		//hide(), toggle()
+		$(this).closest('.item-box').find('.more-info').toggle('slow', function(){
+			alert('finished');
+		});
+
+		//fadeIn(), fadeOut(), fadeToggle()
+		$(this).closest('.item-box').find('.more-info').fadeToggle('slow');
+
+		//slideDown(), slideUp(), slideToggle()
+		$(this).closest('.item-box').find('.more-info').slideToggle('slow');
+
+		//animation for CSS Properties
+		$('#example1').on('click', function(){
+			$(this).animate({
+				opacity: 0.3
+			}, 500, function(){
+				alert('completed!');
+			});
+		});
+	
+		$('#example2').on('click', function(){
+			$(this).animate({
+				left: 200,
+				width: 40
+			}, 500);
+		});
+	
+		$('#example3').on('click', function(){
+			$(this).animate({
+				left: '+=20',
+				width: '+=50'
+			}, 500);
+		});
+	});
+
+
+
+	/* 
+	OTHERS:
+		- AJAX
+	 */
 });
 
 
